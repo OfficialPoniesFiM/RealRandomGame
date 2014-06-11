daf3b65041010f28eac954a8edf87a802c9075cf
OfficialPoniesFiM <517b2730@opayq.com>
2014-06-11 16:47:56 -0700

    Make script executable. Python 2.7
    Make script executable for Linux. Python 2.7

--- main_python27.py
+++ main_python27.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #This is a simple game. 
 #Copyright (C) 2014 Nathan Guerrero/PoniesFiM
 """This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
