12c1dbefc4b3dc021542020ce27356c53ea183ed
Nathan Guerrero <poniesfim@hushmail.com>
2016-07-01 17:31:29 -0700

    Change collision areas

--- main.py
+++ main.py
@@ -348,9 +348,9 @@ while whiles:
             toasty.play() #Play this sound.
         
         twoPlay = False #Set this variable to false to make sure sounds don't play again until enemy Sniper gets hit again.
-    if characterX > otherX2: #If the Creeper approaches the character,
+    if characterX > otherX2 - 125: #If the Creeper approaches the character,
         rekt = True #set rekt to true, to make the game know that it has to do things.
-    elif characterX < otherX1: #If the enemy Sniper approaches the character,
+    elif characterX < otherX1 + 200: #If the enemy Sniper approaches the character,
         rekt = True #set rekt to true, to make the game know that it has to do things.
     
     #Due to issues with the sniper, we have to redraw the thing every refresh.
